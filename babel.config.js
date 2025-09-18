f842dd9607e359b4e8b6eb8e15b7f649060e05f6
Vaibhav Shrivastava <vaibhavshrivastavait@gmail.com>
2025-09-18 01:29:41 +0000

    auto-commit for 102efd3e-c864-4b9e-951f-d0fcfdc1d375

--- babel.config.js
+++ babel.config.js
@@ -2,20 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ['babel-preset-expo', { 
-        jsxImportSource: '@emotion/react',
-        jsxRuntime: 'automatic',
-      }],
+      'babel-preset-expo',
     ],
     plugins: [
-      // Essential for modern JS support
-      ['@babel/plugin-proposal-optional-chaining', { loose: false }],
-      ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
-      ['@babel/plugin-proposal-logical-assignment-operators'],
-      // Existing plugins
       ['@babel/plugin-proposal-class-properties', { loose: true }],
       ['@babel/plugin-transform-private-methods', { loose: true }],
       ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+      ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+      ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
       // NOTE: react-native-reanimated/plugin must be listed last
       'react-native-reanimated/plugin',
     ]
