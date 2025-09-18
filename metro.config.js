b018717aa25c4d7af4252d2f7492a63803ee1845
Vaibhav Shrivastava <vaibhavshrivastavait@gmail.com>
2025-09-18 04:22:23 +0000

    auto-commit for faf00e4c-f0dd-444a-b18e-f8d66f29d62f

--- metro.config.js
+++ metro.config.js
@@ -5,18 +5,19 @@ const config = getDefaultConfig(__dirname);
 
 // Minimal config to avoid file watcher issues
 config.resolver.blacklistRE = /(node_modules\/.*\/node_modules\/.*|__tests__\/.*|.*\/__tests__\/.*)/;
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+config.resolver.platforms = ['ios', 'android', 'native'];
 
 // Disable unnecessary features
 config.serializer.customSerializer = undefined;
 config.transformer.minifierConfig = { sourceMap: false };
 
-// Limit watched folders
+// Limit watched folders to essential app directories only
 config.watchFolders = [
   path.resolve(__dirname, 'screens'),
   path.resolve(__dirname, 'components'),
   path.resolve(__dirname, 'utils'),
   path.resolve(__dirname, 'store'),
+  path.resolve(__dirname, 'app'),
 ];
 config.maxWorkers = 1;
 
