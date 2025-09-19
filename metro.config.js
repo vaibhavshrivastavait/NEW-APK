36ff3b7f3e3d5ce617a365c9eea6065149bb0c2e
Vaibhav Shrivastava <vaibhavshrivastavait@gmail.com>
2025-09-19 01:00:17 +0000

    auto-commit for 3a4ed7a3-8e81-42ba-82fb-dbe665cc7f0b

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-// Minimal config to avoid file watcher issues
-config.resolver.blacklistRE = /(node_modules\/.*\/node_modules\/.*|__tests__\/.*|.*\/__tests__\/.*)/;
-config.resolver.platforms = ['ios', 'android', 'native'];
+// Add web platform for better compatibility
+config.resolver.platforms = ['ios', 'android', 'web', 'native'];
 
-// Disable unnecessary features
-config.serializer.customSerializer = undefined;
-config.transformer.minifierConfig = { sourceMap: false };
-
-// Limit watched folders to essential app directories only
-config.watchFolders = [
-  path.resolve(__dirname, 'screens'),
-  path.resolve(__dirname, 'components'),
-  path.resolve(__dirname, 'utils'),
-  path.resolve(__dirname, 'store'),
-  path.resolve(__dirname, 'app'),
-];
-config.maxWorkers = 1;
+// Remove restrictive configurations that might cause issues
+config.maxWorkers = 2;
 
 module.exports = config;
